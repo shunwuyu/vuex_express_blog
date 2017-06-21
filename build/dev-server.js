f65b5a588299c3002ade13a9d7d51e1ba87dcd45
喻顺武 <shunwu2001@163.com>
2017-06-21 16:21:44 +0800

    dev middleware

--- build/dev-server.js
+++ build/dev-server.js
@@ -3,12 +3,14 @@ if(!process.env.NODE_ENV) {
   process.env.NODE_ENV =
     JSON.parse(config.dev.env.NODE_ENV)
 }
+var opn = require('opn')
 // console.log(process.env.NODE_ENV)
 var express = require('express')
 var webpack = require('webpack')
 var proxyMiddleware =
   require('http-proxy-middleware')
-
+var autoOpenBrowser =
+  !!config.dev.autoOpenBrowser
 var port =
  process.env.PORT || config.dev.port
 var proxyTable = config.dev.proxyTable
@@ -22,9 +24,40 @@ var webpackConfig =
 // webpack  babel loader
 // vue-router spa
 var app = express()
+
+var compiler = webpack(webpackConfig)
+// console.log(compiler)
+var devMiddleware =
+ require('webpack-dev-middleware')(compiler, {
+   publicPath: webpackConfig.output.publicPath,
+   quiet: true
+ })
+var hotMiddleware =
+ require('webpack-hot-middleware')(compiler,{
+   log: () => {}
+ })
+compiler.plugin('compilation',
+ function(compilation) {
+   compilation.plugin
+    ('html-webpack-plugin-after-emit',
+     function(data, cb) {
+       hotMiddleware.publish({ action: 'reload'})
+       cb()
+     })
+ })
+
+app.use(devMiddleware)
+app.use(hotMiddleware)
+
+const uri = 'http://localhost:' + port
 app.listen(port, function(err) {
   if (err) {
     console.log(err)
     return
   }
+  if(autoOpenBrowser
+    && process.env.NODE_ENV !== 'testing') {
+      // 打开浏览器 uri  地址
+      opn(uri)
+    }
 })
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,9 +1,18 @@
 var webpack = require('webpack')
+var config = require('../config')
 var baseWebpackConfig =
  require('./webpack.base.conf')
 var merge = require('webpack-merge')
+var HtmlWebpackPlugin =
+  require('html-webpack-plugin')
+
+
 module.exports = merge(baseWebpackConfig,{
+  devtool: '#cheap-module-eval-source-map',
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
